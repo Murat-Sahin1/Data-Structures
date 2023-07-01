fcaa2b120ddfd449014b77f4d64f6babcdaefb20
Murat Şahin <murat.sahin.dev@gmail.com>
2023-07-02 02:12:13 +0300

    Linked list insertion.

--- Linked-List.c
+++ Linked-List.c
@@ -1,40 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Main node struct
 typedef struct n node;
 struct n {
   int data;
   node *next;
 };
 
-// void test_fill_items(node **head) {
-//   node **temp = head;
-//   for (int i = 0; i < 10; i++) {
-//     (*temp)->next = (node *)malloc(sizeof(node));
-//     (*temp)->data = i;
-//     // printf("%d", (*temp)->data);
-//     (*temp)->next->next = NULL;
-//     (*temp) = (*temp)->next;
-//   }
-//   (*temp)->next = NULL;
-// }
+int length = 0;
 
-// void print_list(node **head) {
-//   node **temp = head;
-//   int i;
-//   printf("\nYour list: ");
-//   while (i != 10) {
-//     printf("%d ->", (*temp)->data);
-//     *temp = (*temp)->next;
-//     i++;
-//   }
-//   printf("%d", (*temp)->data);
-// }
+// MAIN FUNCTIONALITY
+
+void insert(node **head, int x, int index) {
+  if ((index > length) || (index < 0)) {
+    printf("\nIndex out of bounds.");
+    return;
+  }
+
+  // NOTE : Check if are we inserting to root or the head is null
+  if (index == 0 || *head == NULL) {
+    node *temp = (node *)malloc(sizeof(node));
+    if (*head == NULL) {
+      temp->next = NULL;
+    } else {
+      temp->next = *head;
+    }
+    temp->data = x;
+    *head = temp;
+    length++;
+    return;
+  }
+
+  node *trav = *head;
+  for (int i = 0; i != index - 1; i++) {
+    trav = trav->next;
+  }
+  node *temp = (node *)malloc(sizeof(node));
+  temp->data = x;
+  temp->next = trav->next;
+  trav->next = temp;
+  length++;
+}
+
+void add(node **head, int x) {}
+
+// UTILITIES
 
 void print_list(node *head) {
   node *temp = head;
   printf("\nYour list: ");
-  while (temp->next->next != NULL) {
+  while (temp->next != NULL) {
     printf("%d ->", temp->data);
     temp = temp->next;
   }
@@ -49,12 +65,32 @@ void test_fill_items(node *head) {
     // printf("%d", (*temp)->data);
     temp->next->next = NULL;
     temp = temp->next;
+    length++;
   }
 }
 
+void print_length() { printf("\nLinked List Length: %d", length); }
+
 int main() {
-  node *head = (node *)malloc(sizeof(node));
-  test_fill_items(head);
+  node *head = NULL; /* (node *)malloc(sizeof(node)); */
+  // test_fill_items(head);
+  // print_list(head);
+  // NOTE : Item will be at the specified index
+  insert(&head, 42, 0);
+  printf("%d", head->data);
   print_list(head);
+  insert(&head, 42, 1);
+  print_list(head);
+  insert(&head, 42, 2);
+  print_list(head);
+  print_length();
+  insert(&head, 11, 23);
+  print_list(head);
+  insert(&head, 238, 0);
+  print_list(head);
+  // insert(&head, 42, 5);
+  // print_list(head);
+  // insert(&head, 12, 0);
+  // print_list(head);
   return 0;
 }
