97a9b7fa7d914241d9d1eefc55d0c37b4fa7e0a7
Murat Şahin <murat.sahin.dev@gmail.com>
2024-06-17 23:59:53 +0300

    Inital hash map

---
+++ Hash-Table/DisplayHashTable/Program.cs
@@ -0,0 +1,9 @@
+﻿using HashTable;
+
+var hashTable = new HashTable.HashTable(10);
+
+// Testing the hash function
+/*
+int hash = hashTable.hashFunction(Testing");
+Console.WriteLine(hash);
+*/
---
+++ Hash-Table/HashTable/HashTable.cs
@@ -0,0 +1,31 @@
+﻿namespace HashTable;
+
+/// <summary>
+/// This is a implementation of HashTable.
+/// It is designed as following: string key, object value
+/// </summary>
+public class HashTable
+{
+    private List<object> baseList;
+
+    public HashTable(int size)
+    {
+        // TODO: Make this a dynamic array that doubles in size when filled %70
+        baseList = new List<object>(size);
+    }
+
+    public bool Set(string key, object value)
+    {
+        throw new NotImplementedException();
+    }
+
+private int hashFunction(string key)
+    {
+        var hash = 0;
+        for (var i = 0; i < key.Length; i++)
+        {
+            hash = (hash + key.ElementAt(i) * i) % baseList.Capacity;
+        }
+        return hash;
+    }
+}
