2541d4e6a48429bd73be7fd55bc0bedf9aec43ae
Murat Şahin <murat.sahin.dev@gmail.com>
2024-06-18 03:28:47 +0300

    Second hash map

--- Hash-Table/HashTable/HashTable.cs
+++ Hash-Table/HashTable/HashTable.cs
@@ -6,26 +6,56 @@
 /// </summary>
 public class HashTable
 {
-    private List<object> baseList;
+    private HashTableEntry[] _baseArray;
+    private int _capacity;
 
     public HashTable(int size)
     {
-        // TODO: Make this a dynamic array that doubles in size when filled %70
-        baseList = new List<object>(size);
+        if (size < 5)
+        {
+            size = 5;
+        }
+        _capacity = size;
+        _baseArray = new HashTableEntry[size];
     }
 
-    public bool Set(string key, object value)
+    public bool Set(string? key, object value)
     {
-        throw new NotImplementedException();
+        if (key == null)
+        {
+            return false;
+        }
+        bool onCapacity = checkThreshold();
+        var newEntry = new HashTableEntry { key = key, value = value };
+        var arrayIndex = hashFunction(newEntry.key);
+        // TODO: Handle the collision 💀
+        _baseArray[arrayIndex] = newEntry;
+        return true;
     }
 
-private int hashFunction(string key)
+    private int hashFunction(string? key)
     {
         var hash = 0;
         for (var i = 0; i < key.Length; i++)
         {
-            hash = (hash + key.ElementAt(i) * i) % baseList.Capacity;
+            hash = (hash + key.ElementAt(i) * i) % _baseArray.Length;
         }
         return hash;
+    }
+
+    /// <summary>
+    /// Checks if the base array length reached the threshold capacity.
+    /// Which is set to %70 of array capacity. This might not be the best option.
+    /// Implementing a percentage that's smaller with low array capacities is better.  
+    /// </summary>
+    /// <returns>True if the base array threshold is reached.</returns>
+    private bool checkThreshold()
+    {
+        int limit = (int)Math.Ceiling(_capacity * 0.7f);
+        if (_baseArray.Length >= limit)
+        {
+            // Double the size of base array field
+            // modify the capacity private field
+        }
     }
 }
---
+++ Hash-Table/HashTable/HashTableEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace HashTable;
+
+/// <summary>
+/// Entry for HashTable base array.
+/// </summary>
+public class HashTableEntry
+{
+    public string? key { get; init; }
+    public object value { get; set; }
+}
